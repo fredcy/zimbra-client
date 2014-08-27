5f0a7aa45ac24468b1a46bf7ddf1539f5470fac3
Fred Yankowski <fcy@imsa.edu>
2014-08-27 09:59:15 -0500

    Commands working, both with context and with parameters.

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 import logging
 import os
 import pprint
+import json
+import argparse
+import urllib2
+# github.com/Zimbra-Community/python-zimbra
 from pythonzimbra.tools import auth
 from pythonzimbra.request_xml import RequestXml
 from pythonzimbra.response_xml import ResponseXml
@@ -9,26 +13,52 @@ from pythonzimbra.communication import Communication
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-url = os.environ['ZIMBRA_URL']
-account = os.environ['ZIMBRA_ACCOUNT']
-password = os.environ['ZIMBRA_PASSWORD']
-preauthkey = os.environ['ZIMBRA_PREAUTHKEY']
+class Zimbra():
+    def __init__(self):
+        self.comm = None
+        self.token = None
 
-comm = None
-token = None
+    def connect(self):
+        url = os.environ['ZIMBRA_URL']
+        account = os.environ['ZIMBRA_ACCOUNT']
+        password = os.environ['ZIMBRA_PASSWORD']
+        try:
+            self.comm = Communication(url)
+            self.token = auth.authenticate(url, account, password, admin_auth=True)
+        except:
+            log.exception("locals=%s", locals())
+        if not self.token:
+            raise Exception("Authentication failed: locals=%s", locals())
 
-def connect():
-    global comm
-    global token
-    comm = Communication(url)
-    token = auth.authenticate(
-        url,
-        account,
-        password,
-        admin_auth=True,
-    )
-    if not token:
-        raise Exception("Authentication failed")
+    def request(self, request_name, params=None, context=None):
+        request = RequestXml()
+        try:
+            urn = 'urn:zimbraAccount' if context and 'account' in context else 'urn:zimbraAdmin'
+            request.add_request(request_name, params or {}, urn)
+            if context:
+                request.set_context_params(context)
+            request.set_auth_token(self.token)
+        except:
+            log.exception("failed to build request: request_name=%s, params=%s, context=%s",
+                          request_name, params, context)
+            return None
+        log.debug("request = %s", request.get_request())
+
+        response = ResponseXml()
+        try:
+            self.comm.send_request(request, response)
+        except urllib2.HTTPError as e:
+            log.error("send_request HTTP Error %s: %s; request=%s",
+                          e.code, e.reason, request.get_request())
+            return None
+        except Exception as e:
+            log.exception("send_request failed (%s): request=%s", type(e), request.get_request())
+            return None
+        if response.is_fault():
+            log.error("send_request returned fault: request=%s", request.get_request())
+            return None
+        info = response.get_response()
+        return info
 
 def getalladminaccounts():
     request = RequestXml()
@@ -131,22 +161,34 @@ def getcalendarresource():
     log.debug("request = %s", request.get_request())
 
     response = ResponseXml()
-    comm.send_request(request, response)
+    try:
+        comm.send_request(request, response)
+    except:
+        log.exception("send_request failed: request=%s", request.get_request())
+        return none
     if response.is_fault():
         raise Exception("response is fault")
     info = response.get_response()
     pprint.pprint(info)
 
 def main():
-    connect()
-    #getallservers()
-    #getallaccounts()
-    #getallcalendarresources()
-    #getcalendarresource()
-    mboxes = getallmailboxes()
-    for mbox in mboxes[100:101]:
-        mailbox = getmailbox(mbox['accountId'])
-        pprint.pprint(mailbox)
+    parser = argparse.ArgumentParser(description="Make Zimbra SOAP requests")
+    parser.add_argument('request', help='Name of request')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--account', help='Account name for context')
+    parser.add_argument('--params', help='Parameters of request')
+    parser.add_argument('--depth', type=int,  default=None, help='Depth of structure prints')
+    args = parser.parse_args()
+
+    log.setLevel(logging.DEBUG if args.debug else logging.INFO)
+
+    z = Zimbra()
+    z.connect()
+
+    context = { 'account': { '_content': args.account, 'by': 'name' } } if args.account else None
+    request_name = args.request if args.request.endswith('Request') else args.request + 'Request'
+    params = json.loads(args.params) if args.params else None
+    pprint.pprint(z.request(request_name, params=params, context=context), depth=args.depth)
 
 if __name__ == "__main__":
     main()
